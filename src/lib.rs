9035fb5b392387542e8bca91451594b836e17d09
mikasd <michael.ingley@gmail.com>
2019-11-20 09:40:44 -0800

    implements primitive multithreading

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,32 @@
 use std::thread;
+use std::sync::mpsc;
+use std::sync::Mutex;
+use std::sync::Arc;
 
 pub struct ThreadPool{
-    threads: Vec<thread::JoinHandle<()>>,
+    workers: Vec<Worker>,
+    sender: mspc::Sender<Job>,
 }
+
+struct Job;
 
 impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool{
         assert!(size > 0);
 
-        let mut threads = Vec::with_capacity(size);
+        let (sender, reciever) = mpsc::channel();
 
-        for _ in 0..size{
-            //TODO: create some threads and store them in vector
+        let reciever = Arc::new(Mutex::new(reciever));
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&reciever)));
         }
 
         ThreadPool{
-            threads;
+            workers,
+            sender,
         }
     }
 
@@ -24,5 +35,23 @@ impl ThreadPool {
             F: FnOnce() + Send + 'static
     {
 
+    }
+}
+
+struct Worker{
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize, reciever: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(|| {
+            reciever;
+        });
+
+        Worker{
+            id,
+            thread,
+        }
     }
 }
